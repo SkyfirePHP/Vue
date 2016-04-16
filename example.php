f3c3afa1bebf0fc6664bc32a9b0a759b7bb84dd7
Travis van der Font <travis.font@gmail.com>
2016-04-16 14:30:09 +0200

    added enum value altering (not fully completed)

--- example.php
+++ example.php
@@ -20,3 +20,16 @@ $query = $SQL->SELECT('r.rid, r.name')
              ->ORDER_BY('r.weight')->ASC();
 
 echo $query->exe();
+
+
+/*
+$add   = new VueSQL($conn);
+$query = $add->ALTER_TABLE('xml_mapping')
+              ->CHANGE('special_rule')
+              ->ENUM('meta_value_default',
+                     'convert_boolean',
+                     'multiple_value_terms')
+              ->CHARACTER_SET('utf8')
+              ->COLLATE('utf8_general_ci')
+              ->NULL();
+*/
--- src/VueSQL.php
+++ src/VueSQL.php
@@ -40,12 +40,60 @@ class ALTER_DATABASE extends method
     }
 }
 
+class CHANGE_TABLE
+{
+    private $query;
+
+    public function __construct($query)
+    {
+        $this->query = $query;
+    }
+
+    public function CHANGE($field_name)
+    {
+        $this->query .= 'CHANGE `'.$field_name.'` `'.$field_name.'`';
+
+        return $this;
+    }
+
+    public function ENUM($enum)
+    {
+        $enum = array(); // get the values from the all the parameters
+
+        // add a " to both sides of the enum using a array_map()
+        $this->query .= ' ENUM('.implode(',', $enum).')';
+
+        return $this;
+    }
+
+    public function CHARACTER_SET($character_type)
+    {
+        $this->query .=  'CHARACTER SET '.$character_type;
+
+        return $this;
+    }
+
+    public function COLLATE($character_type)
+    {
+        $this->query .=  'COLLATE '.$character_type;
+
+        return $this;
+    }
+
+    public function NULL()
+    {
+        $this->query .=  'NULL';
+    }
+}
+
 class ALTER_TABLE extends ALTER_DATABASE
 {
     // start statement
     public function ALTER_TABLE($table_name)
     {
         $this->query = 'ALTER TABLE '.self::cleanSmt($table_name);
+
+        return new CHANGE_TABLE($this->query);
     }
 
     public function CONVERT_TO_CHARACTER_SET($character_set)
